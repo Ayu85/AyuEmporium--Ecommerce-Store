9a801d7e30744e3a28c3e470043ecf26f40fd278
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-12 19:57:59 +0530

    why us section designed now animating

--- ayu-emporium/src/components/Body.js
+++ ayu-emporium/src/components/Body.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from './Header'
 import Herosection from './Herosection'
 import Slidingtext from './Slidingtext'
+import Whyus from './Whyus'
 
 const Body = () => {
     return (
         <div className='overflow-hidden'>
             <Herosection />
             <Slidingtext/>
+            <Whyus/>
         </div>
     )
 }
--- ayu-emporium/src/components/Whyus.js
+++ ayu-emporium/src/components/Whyus.js
@@ -6,30 +6,31 @@ import { RiSecurePaymentLine } from "react-icons/ri";
 
 const Whyus = () => {
     return (
-        <div className='flex items-center justify-between'>
-            <div className='flex flex-col gap-3'>
-                <BsTextareaResize />
-                <h1>Fit or Free</h1>
-                <h1>Our Products Always Fits You In Every Shape</h1>
+        <div className='flex items-center justify-between bg-[#faf2e5] py-4 px-10'>
+            <div className='flex flex-col gap-2 items-center rounded-xl justify-center border-slate-600 border px-3 py-4 w-80 text-center '>
+                <BsTextareaResize className='text-4xl' />
+                <h1 className='text-xl font-semibold text-slate-600'>Fit or Free</h1>
+                <h1 className='text-slate-600 '>Our Products Always Fits You In Every Shape</h1>
             </div>
 
-            <div>
-                <RiSecurePaymentLine />
-                <h1>Secure Payments</h1>
-                <h1>Experience Hassle Free Payments Throughout Checkout Process</h1>
-
-            </div>
-
-            <div>
-                <CiDeliveryTruck />
-                <h1>Fastest Delivery</h1>
-                <h1>7 Days Delivery In Almost All Cities Across India</h1>
+            <div className='flex flex-col gap-2 items-center rounded-xl justify-center border-slate-600 border px-3 py-4 w-80 text-center '>
+                <RiSecurePaymentLine className='text-4xl' />
+                <h1 className='text-xl font-semibold text-slate-600'>Secure Payments</h1>
+                <h1 className='text-slate-600 '>Experience Hassle Free Payments Throughout Checkout Process</h1>
 
             </div>
 
-            <div>
-                <GiReturnArrow />
-                <h1>Easy Returns</h1>
+            <div className='flex flex-col gap-2 items-center rounded-xl justify-center border-slate-600 border px-3 py-4 w-80 text-center '>
+                <CiDeliveryTruck className='text-4xl' />
+                <h1 className='text-xl font-semibold text-slate-600'>Fastest Delivery</h1>
+                <h1 className='text-slate-600 '>7 Days Delivery In Almost All Cities Across India</h1>
+
+            </div>
+
+            <div className='flex flex-col gap-2 items-center rounded-xl justify-center border-slate-600 border px-3 py-4 w-80 text-center '>
+                <GiReturnArrow className='text-4xl' />
+                <h1 className='text-xl font-semibold text-slate-600'>Easy Returns</h1>
+                <h1 className='text-slate-600 '>Hassle Free Returns and Replacement Within 10 Days </h1>
             </div>
 
         </div>
