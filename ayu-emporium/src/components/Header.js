720b6039ab54c92e5e566d934a4d821ba8de3d73
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-12 12:16:10 +0530

    header

--- ayu-emporium/src/components/Header.js
+++ ayu-emporium/src/components/Header.js
@@ -2,17 +2,17 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <div className='flex items-center justify-between py-2 px-10' >
-            <div><h1 className='page_title text-3xl'>AyuEmporium</h1></div>
+        <div className='flex items-center justify-between py-3 px-10 bg-[#1c223d] ' >
+            <div><h1 className='page_title text-3xl text-[#eab99f]'>AyuEmporium</h1></div>
             <div >
-                <ul className='flex gap-4 font-semibold text-lg'> {
+                <ul className='flex gap-4  text-lg text-white tracking-wide cursor-pointer'> {
                     ['Home', 'Shop', 'Carrers', 'About'].map((list_items) => {
-                        return <li>{list_items}</li>
+                        return <li className=' hover:border-b border-[#eab99f] transition-all '>{list_items}</li>
                     })
                 }</ul>
             </div>
             <div>
-                
+                <button className='px-4 py-1 rounded-lg bg-[#e1586c] font-semibold '>Log In</button>
             </div>
         </div>
     )
