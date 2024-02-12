8ca306104eaa578146fafef0da8da78dfa4497c1
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-12 14:12:35 +0530

    hero section done

--- ayu-emporium/src/components/Herosection.js
+++ ayu-emporium/src/components/Herosection.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import heroImg from '../assets/heroImg3.png'
 import Header from './Header'
+import { motion } from 'framer-motion'
 const Herosection = () => {
+
   return (
-    <div className=' flex relative flex-col gap-16 justify-center items-center  h-[100vh] bg-[#FFB71B] '>
+    <div className=' flex relative flex-col gap-16 justify-center items-center  h-[110vh] bg-[#FFB71B] '>
       <Header />
-      <img src='https://cdn.shopify.com/s/files/1/0393/0007/1555/files/christmas_elements-desktop.png?v=1701336847'
-        alt='bg' className='absolute -z-0' />
+      {/* <img src='https://cdn.shopify.com/s/files/1/0393/0007/1555/files/christmas_elements-desktop.png?v=1701336847' */}
+        {/* alt='bg' className='absolute -z-0' /> */}
       <div className='flex items-center justify-between  w-[75%]'>
         <div className='text-black z-50'>
           {
             ['Shop', 'Click', 'Thrive:Your E-Commerce Destination'].map((items, key) => {
-              return <h1 className={`text-7xl hero_title py-1
-               ${key === 1 && 'text-[#F7221E] w-[500px] text-6xl'}
-                 ${key === 2 && 'text-white w-[550px] text-4xl tracking-wider'}`}>{items}</h1>
+              return <h1 className={`text-7xl hero_title py-3
+               ${key === 1 && 'text-[#F7221E]  text-6xl'}
+                ${key === 2 && 'text-white w-[550px] text-4xl tracking-wider'}`}>
+                <motion.span
+                  id='square' className={`${key === 1 && 'bg-[#F7221E] pl-44 h-24 '}`}></motion.span>{items}</h1>
             })
           }
         </div>
