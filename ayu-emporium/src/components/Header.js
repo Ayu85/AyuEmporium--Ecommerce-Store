a455070fc17bc24ad740c7875cd4d8cbb2b88ad6
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-12 12:02:38 +0530

    header designing

--- ayu-emporium/src/components/Header.js
+++ ayu-emporium/src/components/Header.js
@@ -1,11 +1,21 @@
 import React from 'react'
 
 const Header = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className='flex items-center justify-between py-2 px-10' >
+            <div><h1 className='page_title text-3xl'>AyuEmporium</h1></div>
+            <div >
+                <ul className='flex gap-4 font-semibold text-lg'> {
+                    ['Home', 'Shop', 'Carrers', 'About'].map((list_items) => {
+                        return <li>{list_items}</li>
+                    })
+                }</ul>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    )
 }
 
 export default Header
