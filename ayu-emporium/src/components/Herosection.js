f871040d874d1230fb18c6ee3b650a586c8855fa
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-12 14:22:26 +0530

    animation in hero section

--- ayu-emporium/src/components/Herosection.js
+++ ayu-emporium/src/components/Herosection.js
@@ -2,15 +2,16 @@ import React from 'react'
 import heroImg from '../assets/heroImg3.png'
 import Header from './Header'
 import { motion } from 'framer-motion'
+import blob from '../assets/blob vector.png'
 const Herosection = () => {
 
   return (
-    <div className=' flex relative flex-col gap-16 justify-center items-center  h-[110vh] bg-[#FFB71B] '>
+    <div className=' flex  flex-col gap-16 justify-center items-center  h-[110vh] bg-[#FFB71B] '>
       <Header />
       {/* <img src='https://cdn.shopify.com/s/files/1/0393/0007/1555/files/christmas_elements-desktop.png?v=1701336847' */}
-        {/* alt='bg' className='absolute -z-0' /> */}
+      {/* alt='bg' className='absolute -z-0' /> */}
       <div className='flex items-center justify-between  w-[75%]'>
-        <div className='text-black z-50'>
+        <div className='text-black '>
           {
             ['Shop', 'Click', 'Thrive:Your E-Commerce Destination'].map((items, key) => {
               return <h1 className={`text-7xl hero_title py-3
@@ -21,8 +22,9 @@ const Herosection = () => {
             })
           }
         </div>
-        <div className='-mr-16 mt-10 '>
-          <img src={heroImg} alt='logo' className='w-[450px]' />
+        <div className='-mr-16 mt-10 relative'>
+          <img src={heroImg} alt='logo' className='w-[450px] z-50 relative' />
+          <img src={blob} alt='blob' className='absolute w-[450px] top-52  z-10 animate-[spin_linear_3s_infinite]'/>
         </div>
       </div>
     </div>
