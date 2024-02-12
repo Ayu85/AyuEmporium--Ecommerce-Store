f58dad0aa8e870c4dd3bed7d4bec908c655d1923
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-12 19:08:59 +0530

    styling of slider done

--- ayu-emporium/src/components/Slidingtext.js
+++ ayu-emporium/src/components/Slidingtext.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import { SiStyleshare } from "react-icons/si";
+
 const Slidingtext = () => {
     return (
         <div className='bg-[#1C161A] w-max overflow-x-hidden h-56 text-white flex text-8xl font-bold items-center justify-center'>
             {
-                ['SHIRTS', 'JEANS', 'PARTY WEAR', 'ETHINIC WEAR', 'WESTERN','SHIRTS', 'JEANS', 'PARTY WEAR', 'ETHINIC WEAR', 'WESTERN','SHIRTS', 'JEANS', 'PARTY WEAR', 'ETHINIC WEAR', 'WESTERN','SHIRTS', 'JEANS', 'PARTY WEAR', 'ETHINIC WEAR', 'WESTERN','SHIRTS', 'JEANS', 'PARTY WEAR', 'ETHINIC WEAR', 'WESTERN','SHIRTS', 'JEANS', 'PARTY WEAR', 'ETHINIC WEAR', 'WESTERN','SHIRTS', 'JEANS', 'PARTY WEAR', 'ETHINIC WEAR', 'WESTERN','SHIRTS', 'JEANS', 'PARTY WEAR', 'ETHINIC WEAR', 'WESTERN'].map((item) => {
-                    return <h1 className='slider tracking-wide border-t border-b border-slate-200 py-7 '> {item + " "} </h1>
+                ['SHIRTS', 'JEANS', 'PARTY WEAR', 'ETHINIC WEAR', 'WESTERN', 'SHIRTS', 'JEANS', 'PARTY WEAR', 'ETHINIC WEAR', 'WESTERN', 'SHIRTS', 'JEANS', 'PARTY WEAR', 'ETHINIC WEAR', 'WESTERN', 'SHIRTS', 'JEANS', 'PARTY WEAR', 'ETHINIC WEAR', 'WESTERN', 'SHIRTS', 'JEANS', 'PARTY WEAR', 'ETHINIC WEAR', 'WESTERN', 'SHIRTS', 'JEANS', 'PARTY WEAR', 'ETHINIC WEAR', 'WESTERN', 'SHIRTS', 'JEANS', 'PARTY WEAR', 'ETHINIC WEAR', 'WESTERN', 'SHIRTS', 'JEANS', 'PARTY WEAR', 'ETHINIC WEAR', 'WESTERN'].map((item) => {
+                    return <h1 className='slider tracking-wide border-t border-b border-slate-200 py-7 flex items-center'> {item} <SiStyleshare className='text-[#FFB71B] text-5xl' />   </h1>
                 })
             }
         </div>
