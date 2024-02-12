c48710b9f56d50436ebfd92537cf9a65aa3a6509
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-12 22:32:23 +0530

    animation refactored

--- ayu-emporium/src/components/Whyus.js
+++ ayu-emporium/src/components/Whyus.js
@@ -8,7 +8,7 @@ const Whyus = () => {
     return (
         <div className='flex flex-col gap-10  flex-wrap items-center justify-center bg-[#FFF8F4] pt-20 py-4 px-14'>
             <h1 className='text-4xl text-slate-900 font-bold'>Why Choose Us?</h1>
-            <div className='flex gap-5 items-center justify-center'><div className='flex group hover:bg-[#1C161A] hover:scale-105 transition-all hover:text-slate-200 cursor-pointer flex-col gap-2 items-center rounded-xl justify-center border-slate-600 border px-3 py-4 w-80 text-center '>
+            <div className='flex flex-wrap gap-5 items-center justify-center'><div className='flex group hover:bg-[#1C161A] hover:scale-105 transition-all hover:text-slate-200 cursor-pointer flex-col gap-2 items-center rounded-xl justify-center border-slate-600 border px-3 py-4 w-80 text-center '>
                 <BsTextareaResize className='text-4xl' />
                 <h1 className='text-xl font-semibold text-slate-600 group-hover:text-slate-300'>Fit or Free</h1>
                 <h1 className='text-slate-600 group-hover:text-slate-300 '>Our Products Always Fits You In Every Shape</h1>
