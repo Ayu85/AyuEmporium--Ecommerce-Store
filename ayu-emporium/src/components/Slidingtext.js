c411d0046d203a52ef6fb3cf21bcf175c6a8a199
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-12 19:04:31 +0530

    sliding animations added

--- ayu-emporium/src/components/Slidingtext.js
+++ ayu-emporium/src/components/Slidingtext.js
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import { motion } from 'framer-motion'
 const Slidingtext = () => {
     return (
         <div className='bg-[#1C161A] w-max overflow-x-hidden h-56 text-white flex text-8xl font-bold items-center justify-center'>
             {
-                ['SHIRTS', 'JEANS', 'PARTY WEAR', 'ETHINIC WEAR', 'WESTERN'].map((item) => {
-                    return <h1 className='slider tracking-wide'> {item+" "} </h1>
+                ['SHIRTS', 'JEANS', 'PARTY WEAR', 'ETHINIC WEAR', 'WESTERN','SHIRTS', 'JEANS', 'PARTY WEAR', 'ETHINIC WEAR', 'WESTERN','SHIRTS', 'JEANS', 'PARTY WEAR', 'ETHINIC WEAR', 'WESTERN','SHIRTS', 'JEANS', 'PARTY WEAR', 'ETHINIC WEAR', 'WESTERN','SHIRTS', 'JEANS', 'PARTY WEAR', 'ETHINIC WEAR', 'WESTERN','SHIRTS', 'JEANS', 'PARTY WEAR', 'ETHINIC WEAR', 'WESTERN','SHIRTS', 'JEANS', 'PARTY WEAR', 'ETHINIC WEAR', 'WESTERN','SHIRTS', 'JEANS', 'PARTY WEAR', 'ETHINIC WEAR', 'WESTERN'].map((item) => {
+                    return <h1 className='slider tracking-wide border-t border-b border-slate-200 py-7 '> {item + " "} </h1>
                 })
             }
         </div>
