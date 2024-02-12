ee81fed5c758e0a2b3fc833539507977e5717ccd
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-12 20:04:12 +0530

    card animations done

--- ayu-emporium/src/components/Whyus.js
+++ ayu-emporium/src/components/Whyus.js
@@ -7,30 +7,30 @@ import { RiSecurePaymentLine } from "react-icons/ri";
 const Whyus = () => {
     return (
         <div className='flex items-center justify-between bg-[#faf2e5] py-4 px-10'>
-            <div className='flex flex-col gap-2 items-center rounded-xl justify-center border-slate-600 border px-3 py-4 w-80 text-center '>
+            <div className='flex group hover:bg-[#1C161A] hover:scale-105 transition-all hover:text-slate-200 cursor-pointer flex-col gap-2 items-center rounded-xl justify-center border-slate-600 border px-3 py-4 w-80 text-center '>
                 <BsTextareaResize className='text-4xl' />
-                <h1 className='text-xl font-semibold text-slate-600'>Fit or Free</h1>
-                <h1 className='text-slate-600 '>Our Products Always Fits You In Every Shape</h1>
+                <h1 className='text-xl font-semibold text-slate-600 group-hover:text-slate-300'>Fit or Free</h1>
+                <h1 className='text-slate-600 group-hover:text-slate-300 '>Our Products Always Fits You In Every Shape</h1>
             </div>
 
-            <div className='flex flex-col gap-2 items-center rounded-xl justify-center border-slate-600 border px-3 py-4 w-80 text-center '>
+            <div className='flex group hover:bg-[#1C161A] hover:scale-105 transition-all hover:text-slate-200 cursor-pointer flex-col gap-2 items-center rounded-xl justify-center border-slate-600 border px-3 py-4 w-80 text-center '>
                 <RiSecurePaymentLine className='text-4xl' />
-                <h1 className='text-xl font-semibold text-slate-600'>Secure Payments</h1>
-                <h1 className='text-slate-600 '>Experience Hassle Free Payments Throughout Checkout Process</h1>
+                <h1 className='text-xl font-semibold text-slate-600 group-hover:text-slate-300'>Secure Payments</h1>
+                <h1 className='text-slate-600 group-hover:text-slate-300'>Experience Hassle Free Payments Throughout Checkout Process</h1>
 
             </div>
 
-            <div className='flex flex-col gap-2 items-center rounded-xl justify-center border-slate-600 border px-3 py-4 w-80 text-center '>
+            <div className='flex group hover:bg-[#1C161A] hover:scale-105 transition-all hover:text-slate-200 cursor-pointer flex-col gap-2 items-center rounded-xl justify-center border-slate-600 border px-3 py-4 w-80 text-center '>
                 <CiDeliveryTruck className='text-4xl' />
-                <h1 className='text-xl font-semibold text-slate-600'>Fastest Delivery</h1>
-                <h1 className='text-slate-600 '>7 Days Delivery In Almost All Cities Across India</h1>
+                <h1 className='text-xl font-semibold text-slate-600 group-hover:text-slate-300'>Fastest Delivery</h1>
+                <h1 className='text-slate-600 group-hover:text-slate-300 '>7 Days Delivery In Almost All Cities Across India</h1>
 
             </div>
 
-            <div className='flex flex-col gap-2 items-center rounded-xl justify-center border-slate-600 border px-3 py-4 w-80 text-center '>
+            <div className='flex group hover:bg-[#1C161A] hover:scale-105 transition-all hover:text-slate-200 cursor-pointer flex-col gap-2 items-center rounded-xl justify-center border-slate-600 border px-3 py-4 w-80 text-center '>
                 <GiReturnArrow className='text-4xl' />
-                <h1 className='text-xl font-semibold text-slate-600'>Easy Returns</h1>
-                <h1 className='text-slate-600 '>Hassle Free Returns and Replacement Within 10 Days </h1>
+                <h1 className='text-xl font-semibold text-slate-600 group-hover:text-slate-300'>Easy Returns</h1>
+                <h1 className='text-slate-600 group-hover:text-slate-300'>Hassle Free Returns and Replacement Within 10 Days </h1>
             </div>
 
         </div>
