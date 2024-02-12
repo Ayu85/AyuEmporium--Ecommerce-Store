9ba5e02a81f7a0bee5820f3b71c490874d9de449
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-12 15:11:39 +0530

    dispatching an action

--- ayu-emporium/src/components/redux/store.js
+++ ayu-emporium/src/components/redux/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import showCategories from './slices/showCategories'
 const store = configureStore({
-    reducer: {}
+    reducer: {
+        categoriesView: showCategories
+    }
 })
 export default store
