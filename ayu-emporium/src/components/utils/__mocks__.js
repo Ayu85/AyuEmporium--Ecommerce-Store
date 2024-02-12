607e1ff1b36259b4df61432a93f4eaff745c9d0e
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-12 22:10:37 +0530

    mocks for categories made

---
+++ ayu-emporium/src/components/utils/__mocks__.js
@@ -0,0 +1,32 @@
+export const categories = [
+    {
+        logo: '  https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/1376577/2022/6/3/ea10ab6c-883e-437a-8780-ed87484393f81654235830793-Roadster-Men-Black--Grey-Checked-Casual-Sustainable-Shirt-42-1.jpg',
+        name: 'Mens Casual Wear',
+        discount: '50-80% OFF'
+    },
+    {
+        logo: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/24045460/2023/7/17/ae4bf68a-aa78-47f3-970a-eb6c8ea5b0271689567515828KALINIWomenPurpleEthnicMotifsPrintedFlaredSleevesKurta2.jpg',
+        name: 'Women Ethinic Wear',
+        discount: '50-70% OFF'
+    },
+    {
+        logo: 'https://assets.myntassets.com/f_webp,h_560,q_90,w_420/v1/assets/images/19872060/2022/9/22/db2f41e1-292b-43ac-8ec1-17586ab1d39e1663838833622-HELLCAT-Boys-Set-Of-2-Black--Pink-Printed-Hooded-Sweatshirt--7.jpg',
+        name: 'Kids Wear',
+        discount: '50-80% OFF'
+    },
+    {
+        logo: ' https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/25656396/2024/2/6/52ff7b98-0289-4149-b1d6-f511af720c221707204149329-HRX-by-Hrithik-Roshan-Men-Tshirts-7271707204148973-5.jpg',
+        name: 'Sports Wear',
+        discount: '40-80% OFF'
+    },
+    {
+        logo: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/26377366/2023/12/12/782a9df9-be9f-47bc-aa45-7c715f7a5d2f1702374327889MensSelfDesignCottonSherwaniSet1.jpg',
+        name: 'Festive Wear',
+        discount: '50-80% OFF'
+    },
+    {
+        logo: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/7215043/2023/4/17/df2fa87a-821e-455b-b680-acf1450e7e521681708544808-QUIERO-Women-Navy-Blue--Pink-Floral-Printed-Flared-Jumpsuit--1.jpg',
+        name: 'Party Wear',
+        discount: '50-80% OFF'
+    }
+]
