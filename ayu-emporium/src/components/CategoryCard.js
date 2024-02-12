747abd58edd8c3bd2c78329fb3dd0b6d78fa0442
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-12 21:22:37 +0530

    styling of cards

--- ayu-emporium/src/components/CategoryCard.js
+++ ayu-emporium/src/components/CategoryCard.js
@@ -2,10 +2,9 @@ import React from 'react'
 
 const CategoryCard = () => {
     return (
-        <div className='flex flex-col w-80 h-44 '>
+        <div className='flex flex-col w-80 items-center '>
             <img src='https://5.imimg.com/data5/SELLER/Default/2023/7/329252193/LO/EW/HT/2614339/mens-wear.jpg' alt='Logo' />
-            <h1>Mens Wear</h1>
-            <h1>Mens Wear</h1>
+            <h1 className='text-3xl font-semibold text-slate-700'>Mens Wear</h1>
             <h1>50-80% OFF</h1>
         </div>
     )
--- ayu-emporium/src/components/Whyus.js
+++ ayu-emporium/src/components/Whyus.js
@@ -6,7 +6,7 @@ import { RiSecurePaymentLine } from "react-icons/ri";
 
 const Whyus = () => {
     return (
-        <div className='flex gap-5 flex-wrap items-center justify-center bg-[#faf2e5] py-4 px-14'>
+        <div className='flex gap-5 flex-wrap items-center justify-center bg-[#FFF8F4] py-4 px-14'>
             <div className='flex group hover:bg-[#1C161A] hover:scale-105 transition-all hover:text-slate-200 cursor-pointer flex-col gap-2 items-center rounded-xl justify-center border-slate-600 border px-3 py-4 w-80 text-center '>
                 <BsTextareaResize className='text-4xl' />
                 <h1 className='text-xl font-semibold text-slate-600 group-hover:text-slate-300'>Fit or Free</h1>
