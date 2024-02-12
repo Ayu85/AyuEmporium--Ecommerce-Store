3c8a0a51873d6de7c08e2a68618e455bbd4b9b4a
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-12 13:32:33 +0530

    hero sectiopn designied

--- ayu-emporium/src/components/Herosection.js
+++ ayu-emporium/src/components/Herosection.js
@@ -1,20 +1,22 @@
 import React from 'react'
-import heroImg from '../assets/heroImg.png'
+import heroImg from '../assets/heroImg3.png'
 import Header from './Header'
 const Herosection = () => {
   return (
     <div className=' flex flex-col gap-16 justify-center items-center  h-[100vh] bg-[#FFB71B] '>
       <Header />
-      <div className='flex items-center justify-between  '>
+      <div className='flex items-center justify-between  w-[75%]'>
         <div className='text-black z-50'>
           {
             ['Shop', 'Click', 'Thrive:Your E-Commerce Destination'].map((items, key) => {
-              return <h1 className={`text-6xl hero_title  ${key === 2 && 'text-white  text-4xl'}`}>{items}</h1>
+              return <h1 className={`text-7xl hero_title py-1
+               ${key === 1 && 'text-[#F7221E] w-[500px] text-6xl'}
+                 ${key === 2 && 'text-white w-[550px] text-4xl tracking-wider'}`}>{items}</h1>
             })
           }
         </div>
-        <div className='-mr-16'>
-          <img src={heroImg} alt='logo' className='w-[800px]' />
+        <div className='-mr-16 -mt-24'>
+          <img src={heroImg} alt='logo' className='w-[400px]' />
         </div>
       </div>
     </div>
