ab66437afe1f56be28283a9e675087e883e24f2e
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-13 13:39:38 +0530

    hero section of men updated

--- ayu-emporium/src/components/Menswear.js
+++ ayu-emporium/src/components/Menswear.js
@@ -1,18 +1,29 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Herosection from './Herosection'
 import Header, { Header2 } from './Header'
 import { menswear } from './utils/__mocks__'
 import ProductCard from './ProductCard,'
 import { BANNER_URL } from './utils/constants'
 const Menswear = () => {
+    let [currentBanner, setBanner] = useState(0)
     useEffect(() => {
         document.title = 'Mens Wear - AyuEmporium'
+        // setInterval(() => {
+        //     changeBanner()
+        // }, 3000)
     }, [])
+    const changeBanner = () => {
+        if (currentBanner === BANNER_URL.length - 1)
+            setBanner(0)
+        else
+            setBanner(currentBanner++);
+    }
+
     return (
         <div className='flex flex-col items-center justify-center'>
             <Header2 />
-            <div className=' w-full flex justify-center '>
-                <img src={BANNER_URL} alt='banner' className='w-full h-[90vh]' />
+            <div className=' w-full flex justify-center transition-all duration-500'>
+                <img src={BANNER_URL[currentBanner]} alt='banner' className='w-full h-[100vh]' />
             </div>
             <div className='flex gap-5 mt-24 flex-wrap justify-center '>
                 {
--- ayu-emporium/src/components/utils/constants.js
+++ ayu-emporium/src/components/utils/constants.js
@@ -1,1 +1,8 @@
-export const BANNER_URL='https://img.freepik.com/free-photo/big-sale-discounts-products_23-2150336701.jpg?w=996&t=st=1707809948~exp=1707810548~hmac=56611f272c802c1f9c5df6dcaf3ced0107421dc5e3428cd805b18c16e420de5a'
+export const BANNER_URL =
+    ['https://img.freepik.com/free-photo/big-sale-discounts-products_23-2150336701.jpg?w=996&t=st=1707809948~exp=1707810548~hmac=56611f272c802c1f9c5df6dcaf3ced0107421dc5e3428cd805b18c16e420de5a',
+        'https://img.freepik.com/free-vector/abstract-fashion-sale-instagram-stories_52683-25354.jpg?w=996&t=st=1707810925~exp=1707811525~hmac=899d5386e76ab2de0f925a6244acdbd00309e4e5c78c7f167b5cbc5ec0a0534e',
+        'https://img.freepik.com/free-vector/promotion-fashion-banner_1188-201.jpg?w=996&t=st=1707810995~exp=1707811595~hmac=cb1f5936e3e04990c416f32446d455191c98d65c145b0c2a060e5e6e99966a81',
+        'https://img.freepik.com/free-vector/flat-design-fashion-stylist-sale-banner_23-2150007862.jpg?w=1060&t=st=1707811047~exp=1707811647~hmac=8c64a41b80d84d489992552c36869800cb388c662f15973c2db6c37beeb53ad2',
+        'https://img.freepik.com/free-vector/instagram-post-abstract-fashion-sale-set_52683-27048.jpg?w=996&t=st=1707811069~exp=1707811669~hmac=b647551a0b4c548753cefaca5e21f9085629bfb8c10c60a09d50ef3e1d61d4da',
+        'https://img.freepik.com/free-psd/banner-urban-fashion-template_23-2148652497.jpg?w=1380&t=st=1707811094~exp=1707811694~hmac=d315bc55cbd22f17ff09c8b8cb0afa475185baa4e5b731f5a34fb4b02548d174'
+    ]
