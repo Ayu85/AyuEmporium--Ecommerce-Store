c3b0f3b42837ea0277f84a8c36ff0c38ab0fc144
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-13 21:22:27 +0530

    data fetched and display on the page

--- ayu-emporium/src/components/Productdetails.js
+++ ayu-emporium/src/components/Productdetails.js
@@ -3,6 +3,16 @@ import { Header2 } from './Header'
 import { useParams } from 'react-router-dom'
 import { menswear } from './utils/__mocks__'
 const Productdetails = () => {
+
+
+    return (
+        <div>
+            <Header2 />
+            <Details />
+        </div>
+    )
+}
+const Details = () => {
     const prodId = useParams()
     const [id] = useState(prodId.id)
     const [data, setData] = useState(null)
@@ -10,19 +20,24 @@ const Productdetails = () => {
         const d = filterData()
         setData(d[0])
     }, [])
-    console.log(data);
     const filterData = () => {
         return menswear.filter((items) => {
             return items.id == id
         })
     }
-
-    return (
-        <div>
-            <Header2 />
+    return <div className='flex px-5 pt-10'>
+        <div className='flex w-8/12 flex-wrap gap-3'>
+            {/* all 4 images */}
+            <div><img src={data?.display_images[0]} alt='logo' className='w-96' /> </div>
+            <div><img src={data?.display_images[1]} alt='logo' className='w-96' /> </div>
+            <div><img src={data?.display_images[2]} alt='logo' className='w-96' /> </div>
+            <div><img src={data?.display_images[3]} alt='logo' className='w-96' /> </div>
 
         </div>
-    )
+        <div>
+            {/* product details */}
+            details
+        </div>
+    </div>
 }
-
 export default Productdetails
