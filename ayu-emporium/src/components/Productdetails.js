69a901faba5f86a4977d39e8c5c205b61c7357e1
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-13 19:33:10 +0530

    route working perfectly

--- ayu-emporium/src/components/Productdetails.js
+++ ayu-emporium/src/components/Productdetails.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import { Header2 } from './Header'
+import { useParams } from 'react-router-dom'
 
 const Productdetails = () => {
+    const id=useParams()
+    console.log(id);
     return (
         <div>
             <Header2 />
