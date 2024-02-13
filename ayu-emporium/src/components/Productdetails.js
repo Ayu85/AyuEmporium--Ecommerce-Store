dbd626306e94d19b983c1c4c4a14b40ae28638c1
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-13 22:10:53 +0530

    product data rendering on ui

--- ayu-emporium/src/components/Productdetails.js
+++ ayu-emporium/src/components/Productdetails.js
@@ -3,8 +3,6 @@ import { Header2 } from './Header'
 import { useParams } from 'react-router-dom'
 import { menswear } from './utils/__mocks__'
 const Productdetails = () => {
-
-
     return (
         <div>
             <Header2 />
@@ -39,8 +37,14 @@ const Details = () => {
                 <h1 className='text-2xl font-bold text-[#282C3F] uppercase'>{data?.name}</h1>
                 <h1 className='text-[#757884] w-80 text-md font-sans line-clamp-2 '>{data?.about}</h1>
                 <h1 className=' from-black text-md  px-2 py-1 border mt-3 border-slate-300 w-44 text-center'>4.3⭐ | <span className='text-[#757884]'>8K+ Ratings</span></h1>
-
             </div>
+            
+            <div className=' flex items-center gap-4 border-b border-slate-300 pb-3'>
+                <h1 className='text-2xl font-bold text-[#282C3F] uppercase'>Rs. {data?.details?.bestPrice?.price?.discounted}</h1>
+                <h1 className='text-[#757884] text-xl font-sans line-clamp-2 line-through'>MRP Rs.{data?.details?.bestPrice?.price?.mrp}</h1>
+                <h1 className=' text-xl text-[#FFB71B] font-semibold'>(Rs.{data?.details?.bestPrice?.price?.mrp-data?.details?.bestPrice?.price?.discounted} OFF)</h1>
+            </div>
+            
 
         </div>
     </div>
