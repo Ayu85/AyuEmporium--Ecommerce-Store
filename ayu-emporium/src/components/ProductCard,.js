11dbf31c11bfa7b2a765ed85a587332bad4f267d
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-13 15:19:19 +0530

    animations given to cards

--- ayu-emporium/src/components/ProductCard,.js
+++ ayu-emporium/src/components/ProductCard,.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import { HiMiniViewfinderCircle } from "react-icons/hi2";
+
 
 const ProductCard = ({ name, price, discount, about, image }) => {
     return (
         <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 100 }} transition={{ duration: 1.5, ease: 'easeIn' }} className='w-52 flex flex-col'>
-            <div className='relative'>
-                <img src={image} alt='logo' className='w-52' />
+            <div className='relative group overflow-hidden'>
+                <img src={image} alt='logo' className='w-52  cursor-pointer saturate-200 filter hover:brightness-50' />
                 <h1 className='absolute bottom-2 left-3 bg-[#ffffffa2] text-xs font-black px-2 py-1'>4.3⭐ | 8K</h1>
-                <button className='absolute top-1/2 left-1/2 font-black text-lg'>View</button>
+                <button className='absolute group-hover:translate-y-0 transition-all duration-300 hover:z-[999] translate-y-96 top-24 left-16 text-white px-2 py-1 font-black rounded-full text-6xl'><HiMiniViewfinderCircle />
+                </button>
             </div>
             <div className='pl-1'>
                 <h1 className='text-[#282C3F] page_title font-semibold'>{name}</h1>
