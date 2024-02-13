e151548a19b3508d517b66a6970a91abcc3cbe0f
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-13 22:13:17 +0530

    designing

--- ayu-emporium/src/components/Productdetails.js
+++ ayu-emporium/src/components/Productdetails.js
@@ -38,13 +38,16 @@ const Details = () => {
                 <h1 className='text-[#757884] w-80 text-md font-sans line-clamp-2 '>{data?.about}</h1>
                 <h1 className=' from-black text-md  px-2 py-1 border mt-3 border-slate-300 w-44 text-center'>4.3⭐ | <span className='text-[#757884]'>8K+ Ratings</span></h1>
             </div>
-            
-            <div className=' flex items-center gap-4 border-b border-slate-300 pb-3'>
-                <h1 className='text-2xl font-bold text-[#282C3F] uppercase'>Rs. {data?.details?.bestPrice?.price?.discounted}</h1>
-                <h1 className='text-[#757884] text-xl font-sans line-clamp-2 line-through'>MRP Rs.{data?.details?.bestPrice?.price?.mrp}</h1>
-                <h1 className=' text-xl text-[#FFB71B] font-semibold'>(Rs.{data?.details?.bestPrice?.price?.mrp-data?.details?.bestPrice?.price?.discounted} OFF)</h1>
+
+            <div className='flex flex-col gap-2 border-b border-slate-300 pb-3'>
+                <div className=' flex items-center gap-4 '>
+                    <h1 className='text-2xl font-bold text-[#282C3F] uppercase'>Rs. {data?.details?.bestPrice?.price?.discounted}</h1>
+                    <h1 className='text-[#757884] text-xl font-sans line-clamp-2 line-through'>MRP Rs.{data?.details?.bestPrice?.price?.mrp}</h1>
+                    <h1 className=' text-xl text-[#FFB71B] font-semibold'>(Rs.{data?.details?.bestPrice?.price?.mrp - data?.details?.bestPrice?.price?.discounted} OFF)</h1>
+                </div>
+                <h1 className='text-[#03A685] font-semibold'>Incl. of all taxes</h1>
             </div>
-            
+
 
         </div>
     </div>
