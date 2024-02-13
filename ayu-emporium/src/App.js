650fb79ef691e6ed4664d3b65cf0632ce4601846
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-13 11:49:06 +0530

    creating routes

--- ayu-emporium/src/App.js
+++ ayu-emporium/src/App.js
@@ -1,6 +1,18 @@
 import Body from "./components/Body";
+import Menswear from "./components/Menswear";
 import store from "./components/redux/store";
 import { Provider } from 'react-redux'
+import { createBrowserRouter } from 'react-router-dom'
+const approut = createBrowserRouter([
+  {
+    path: '/',
+    element: <Body />
+  },
+  {
+    path: '/menswear',
+    element: <Menswear />
+  }
+])
 function App() {
   return (
     <Provider store={store}>
