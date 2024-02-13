2f6b5e07b50ba70312828a56d21be41644ed9ed8
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-13 11:51:04 +0530

    routing done

--- ayu-emporium/src/App.js
+++ ayu-emporium/src/App.js
@@ -2,8 +2,8 @@ import Body from "./components/Body";
 import Menswear from "./components/Menswear";
 import store from "./components/redux/store";
 import { Provider } from 'react-redux'
-import { createBrowserRouter } from 'react-router-dom'
-const approut = createBrowserRouter([
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+const approute = createBrowserRouter([
   {
     path: '/',
     element: <Body />
@@ -16,7 +16,7 @@ const approut = createBrowserRouter([
 function App() {
   return (
     <Provider store={store}>
-      <Body />
+      <RouterProvider router={approute} />
     </Provider>
   );
 }
