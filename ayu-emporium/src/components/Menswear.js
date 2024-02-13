6c8d719c0299b00cd95f315c6d205043cbf7e21d
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-13 11:57:29 +0530

    dynamic title

--- ayu-emporium/src/components/Menswear.js
+++ ayu-emporium/src/components/Menswear.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Herosection from './Herosection'
+import Header from './Header'
 
 const Menswear = () => {
+    useEffect(() => {
+        document.title = 'Mens Wear - AyuEmporium'
+    }, [])
     return (
-        <div>
-            <Herosection />
+        <div className='flex items-center justify-center'>
+            <Header />
         </div>
     )
 }
