9d7e42f0fd95be8c3d9f7923a378042b4b5ea4bd
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-14 18:33:02 +0530

    bill sumary

--- ayu-emporium/src/components/Cart.js
+++ ayu-emporium/src/components/Cart.js
@@ -3,6 +3,7 @@ import { Header2 } from './Header'
 import { BiSolidOffer } from "react-icons/bi";
 import { useSelector } from 'react-redux';
 import { MdDeleteForever } from "react-icons/md";
+import { MdCurrencyRupee } from "react-icons/md";
 
 const Cart = () => {
     return (
@@ -15,7 +16,7 @@ const Cart = () => {
 const CartDetails = () => {
     const items = useSelector(store => store.cart.items)
     return <div className='flex justify-center items-center mt-10   '>
-        <div className='w-[60%] bg-slate-200 flex  justify-center h-[80vh] pt-10 '>
+        <div className='w-[60%] bg-slate-200 flex  justify-center h-max py-10'>
             <div className='flex gap-10'>
                 <div className='border-r border-black px-4'>
                     {/* left box with items */}
@@ -35,7 +36,8 @@ const CartDetails = () => {
                                 <div>
                                     <h1 className='page_title'>{prod?.name}</h1>
                                     <h1><span className='page_title'>Size: </span>{prod?.size}</h1>
-                                    <h1><span className='page_title'>Price: </span> {prod?.price}</h1>
+                                    <h1 className='flex items-center'><span className='page_title'>Price:  </span> <MdCurrencyRupee className='pl-1 text-lg'/>
+                                        {prod?.price}</h1>
                                     <button className='text-red-600  text-3xl hover:scale-110 transition-all mt-1 cursor-pointer'><MdDeleteForever />
                                     </button>
                                 </div>
