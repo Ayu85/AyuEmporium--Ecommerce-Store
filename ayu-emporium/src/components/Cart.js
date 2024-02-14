0f874f2838a2b0baf4e8510775a4e42a4b0cc9fb
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-14 16:29:37 +0530

    designing

--- ayu-emporium/src/components/Cart.js
+++ ayu-emporium/src/components/Cart.js
@@ -6,29 +6,32 @@ const Cart = () => {
     return (
         <div>
             <Header2 />
+            <CartDetails />
         </div>
     )
 }
 const CartDetails = () => {
-    return <div>
-        <div className='flex'>
-            <div>
-                {/* left box with items */}
-                <div className='flex flex-col gap-2'>
-                    <h1 className='flex items-center text-lg'><BiSolidOffer className='text-xl' />
-                        Available Offers</h1>
-                    <h2 className='font-sans'>10% instant discount on HDFC bank credit and debit cards</h2>
-                    <h2 className='font-sans'>15% instant discount on SBI Rupay card</h2>
+    return <div className='flex justify-center items-center mt-10 '>
+        <div className='w-[60%] bg-slate-200 flex items-center justify-center py-4 h-[80vh] '>
+            <div className='flex gap-10'>
+                <div className='border-r border-black'>
+                    {/* left box with items */}
+                    <div className='flex flex-col gap-2'>
+                        <h1 className='flex items-center text-lg'><BiSolidOffer className='text-xl' />
+                            Available Offers</h1>
+                        <h2 className='font-sans'>10% instant discount on HDFC bank credit and debit cards</h2>
+                        <h2 className='font-sans'>15% instant discount on SBI Rupay card</h2>
+                    </div>
+                </div>
+                <div>
+                    {/* right box with summary */}
+                    <h1>Order Summary</h1>
+                    <h2>Login to get flat 40% off on first order</h2>
                 </div>
             </div>
-            <div>
-                {/* right box with summary */}
-                <h1>Order Summary</h1>
-                <h2>Login to get flat 40% off on first order</h2>
-            </div>
-        </div>
-        <div></div>
+            <div></div>
 
+        </div>
     </div>
 }
 
