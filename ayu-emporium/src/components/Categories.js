ffd5c7395d65035c1edee21344819b7bcecedd26
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-14 14:30:56 +0530

    making router for categories page

--- ayu-emporium/src/components/Categories.js
+++ ayu-emporium/src/components/Categories.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import CategoryCard from './CategoryCard'
 import { categories } from './utils/__mocks__'
+import { Link } from 'react-router-dom'
 const Categories = () => {
 
   return (
     <div className='bg-[#FFF8F4] flex gap-8 flex-wrap justify-center pt-20 '>
       {categories.map((categ) => {
-        return <CategoryCard {...categ} />
+        return <Link to={`/categories/${categ.id}`}><CategoryCard {...categ} /></Link>
       })}
     </div>
   )
--- ayu-emporium/src/components/utils/__mocks__.js
+++ ayu-emporium/src/components/utils/__mocks__.js
@@ -2,22 +2,26 @@ export const categories = [
     {
         logo: '  https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/1376577/2022/6/3/ea10ab6c-883e-437a-8780-ed87484393f81654235830793-Roadster-Men-Black--Grey-Checked-Casual-Sustainable-Shirt-42-1.jpg',
         name: 'Mens Casual Wear',
-        discount: '50-80% OFF'
+        discount: '50-80% OFF',
+        id: 1
     },
     {
         logo: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/24045460/2023/7/17/ae4bf68a-aa78-47f3-970a-eb6c8ea5b0271689567515828KALINIWomenPurpleEthnicMotifsPrintedFlaredSleevesKurta2.jpg',
         name: 'Women Ethinic Wear',
-        discount: '50-70% OFF'
+        discount: '50-70% OFF',
+        id: 2
     },
     {
         logo: 'https://assets.myntassets.com/f_webp,h_560,q_90,w_420/v1/assets/images/19872060/2022/9/22/db2f41e1-292b-43ac-8ec1-17586ab1d39e1663838833622-HELLCAT-Boys-Set-Of-2-Black--Pink-Printed-Hooded-Sweatshirt--7.jpg',
         name: 'Kids Wear',
-        discount: '50-80% OFF'
+        discount: '50-80% OFF',
+        id: 3
     },
     {
         logo: ' https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/25656396/2024/2/6/52ff7b98-0289-4149-b1d6-f511af720c221707204149329-HRX-by-Hrithik-Roshan-Men-Tshirts-7271707204148973-5.jpg',
         name: 'Sports Wear',
-        discount: '40-80% OFF'
+        discount: '40-80% OFF',
+        id: 4
     },
     {
         logo: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/26377366/2023/12/12/782a9df9-be9f-47bc-aa45-7c715f7a5d2f1702374327889MensSelfDesignCottonSherwaniSet1.jpg',
