9bae21d9d2af61ea2962bebec2601c99cc9ef6e7
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-14 16:56:32 +0530

    adding cart items on cart page

--- ayu-emporium/src/components/Cart.js
+++ ayu-emporium/src/components/Cart.js
@@ -14,13 +14,17 @@ const CartDetails = () => {
     return <div className='flex justify-center items-center mt-10 '>
         <div className='w-[60%] bg-slate-200 flex items-center justify-center py-4 h-[80vh] '>
             <div className='flex gap-10'>
-                <div className='border-r border-black'>
+                <div className='border-r border-black px-4'>
                     {/* left box with items */}
                     <div className='flex flex-col gap-2'>
                         <h1 className='flex items-center text-lg'><BiSolidOffer className='text-xl' />
                             Available Offers</h1>
                         <h2 className='font-sans'>10% instant discount on HDFC bank credit and debit cards</h2>
                         <h2 className='font-sans'>15% instant discount on SBI Rupay card</h2>
+                    </div>
+                    <div className='flex flex-col mt-5 '>
+                        <h1 className='text-xl fw-semibold'>Items In Bag</h1>
+
                     </div>
                 </div>
                 <div>
