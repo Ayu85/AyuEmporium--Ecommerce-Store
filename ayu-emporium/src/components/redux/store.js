c1422d26b0b8c512c0dd2d30e5108afc2bc5b50a
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-14 14:57:07 +0530

    slice provided to store

--- ayu-emporium/src/components/redux/store.js
+++ ayu-emporium/src/components/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import showCategories from './slices/showCategories'
+import cartSlice from './slices/cartSlice'
 const store = configureStore({
     reducer: {
-        categoriesView: showCategories
+        categoriesView: showCategories,
+        cart: cartSlice
     }
 })
 export default store
