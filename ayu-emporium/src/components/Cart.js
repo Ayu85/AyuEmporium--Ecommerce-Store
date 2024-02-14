2aed2d8954b2ca757c727818bd1b328fdc9c5716
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-14 18:26:46 +0530

    cart styling

--- ayu-emporium/src/components/Cart.js
+++ ayu-emporium/src/components/Cart.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Header2 } from './Header'
 import { BiSolidOffer } from "react-icons/bi";
+import { useSelector } from 'react-redux';
+import { MdDeleteForever } from "react-icons/md";
 
 const Cart = () => {
     return (
@@ -11,8 +13,9 @@ const Cart = () => {
     )
 }
 const CartDetails = () => {
+    const items = useSelector(store => store.cart.items)
     return <div className='flex justify-center items-center mt-10 '>
-        <div className='w-[60%] bg-slate-200 flex items-center justify-center py-4 h-[80vh] '>
+        <div className='w-[60%] bg-slate-200 flex  justify-center py-10 h-[80vh] '>
             <div className='flex gap-10'>
                 <div className='border-r border-black px-4'>
                     {/* left box with items */}
@@ -22,9 +25,23 @@ const CartDetails = () => {
                         <h2 className='font-sans'>10% instant discount on HDFC bank credit and debit cards</h2>
                         <h2 className='font-sans'>15% instant discount on SBI Rupay card</h2>
                     </div>
-                    <div className='flex flex-col mt-5 '>
+                    <div className='flex flex-col mt-5 gap-6'>
                         <h1 className='text-xl fw-semibold'>Items In Bag</h1>
+                        {items.map((prod) => {
+                            return <div className='flex gap-4'>
+                                <div>
+                                    <img src={prod?.logo} alt='logo' className='w-20' />
+                                </div>
+                                <div>
+                                    <h1 className='page_title'>{prod?.name}</h1>
+                                    <h1><span className='page_title'>Size: </span>{prod?.size}</h1>
+                                    <h1><span className='page_title'>Price: </span> {prod?.price}</h1>
+                                    <button className='text-red-600  text-3xl hover:scale-110 transition-all mt-1 cursor-pointer'><MdDeleteForever />
+                                    </button>
+                                </div>
 
+                            </div>
+                        })}
                     </div>
                 </div>
                 <div>
