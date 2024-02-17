2c1d452b415e8d0583581a8a52325a6d8704dab8
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-17 11:29:31 +0530

    design plan changes

--- ayu-emporium/src/components/ProfilePage.js
+++ ayu-emporium/src/components/ProfilePage.js
@@ -5,7 +5,11 @@ const ProfilePage = () => {
     <div className='absolute flex justify-center bg-[#00000084] backdrop-blur-md text-white items-center z-[99999] w-screen h-[100vh]'>
       {/* modal */}
       <div>
+        {/* left box in m,odal */}
         <h1>Login</h1>
+      </div>
+      <div>
+        {/* right box in modal */}
       </div>
     </div>
   )
